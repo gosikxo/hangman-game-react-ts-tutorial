a470272312e903fe31dc79c99ba90ddf1774c1a2
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-31 16:28:54 +0100

    Add basic structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./components/HangmanDrawing"
+import { HangmanWord } from "./components/HangmanWord"
+import Keyboard from "./components/Keyboard"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -7,7 +10,27 @@ function App() {
   })
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  return <h1>Hi</h1>
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{
+        fontSize: "2rem",
+        textAlign: "center"
+      }}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
 }
 
 export default App
