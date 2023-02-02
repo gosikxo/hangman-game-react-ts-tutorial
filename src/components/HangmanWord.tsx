d0474042234ee8d54cccda9ca5c0cbbffee02a61
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-02 16:58:31 +0100

    Add hangman word component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 
 export const HangmanWord = () => {
+  const word = "test"
+  const guessedLetters = ['t', "e", "g"]
   return (
     <div style={{
       display: "flex",
       gap: ".25em",
       fontSize: "6rem",
-      fontWeight: "bold",
       textTransform: "uppercase",
       fontFamily: "monospace"
     }}>
-      Test
+      {word.split("").map((letter, index) => (
+        <span style={{
+          borderBottom: ".1em solid black"
+        }}
+          key={index}>
+          <span style={{
+            visibility:
+              guessedLetters.includes(letter) ? "visible" : "hidden"
+          }}>
+            {letter}
+          </span>
+        </span>
+      ))}
     </div>
   )
 }
