7deae086051fd14de3366ff720715c8587c23afc
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-03 18:53:38 +0100

    Finish project

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,30 +1,33 @@
-import React from 'react'
-
 const HEAD = (
-    <div style={{
+    <div
+      style={{
         width: "50px",
         height: "50px",
         borderRadius: "100%",
         border: "10px solid black",
         position: "absolute",
         top: "50px",
-        right: "-30px"
-    }} />
-)
-
-const BODY = (
-    <div style={{
+        right: "-30px",
+      }}
+    />
+  )
+  
+  const BODY = (
+    <div
+      style={{
         width: "10px",
         height: "100px",
         background: "black",
         position: "absolute",
         top: "120px",
-        right: 0
-    }} />
-)
-
-const RIGHT_ARM = (
-    <div style={{
+        right: 0,
+      }}
+    />
+  )
+  
+  const RIGHT_ARM = (
+    <div
+      style={{
         width: "100px",
         height: "10px",
         background: "black",
@@ -32,12 +35,14 @@ const RIGHT_ARM = (
         top: "150px",
         right: "-100px",
         rotate: "-30deg",
-        transformOrigin: "left bottom"
-    }} />
-)
-
-const LEFT_ARM = (
-    <div style={{
+        transformOrigin: "left bottom",
+      }}
+    />
+  )
+  
+  const LEFT_ARM = (
+    <div
+      style={{
         width: "100px",
         height: "10px",
         background: "black",
@@ -45,12 +50,14 @@ const LEFT_ARM = (
         top: "150px",
         right: "10px",
         rotate: "30deg",
-        transformOrigin: "right bottom"
-    }} />
-)
-
-const RIGHT_LEG = (
-    <div style={{
+        transformOrigin: "right bottom",
+      }}
+    />
+  )
+  
+  const RIGHT_LEG = (
+    <div
+      style={{
         width: "100px",
         height: "10px",
         background: "black",
@@ -58,12 +65,14 @@ const RIGHT_LEG = (
         top: "210px",
         right: "-90px",
         rotate: "60deg",
-        transformOrigin: "left bottom"
-    }} />
-)
-
-const LEFT_LEG = (
-    <div style={{
+        transformOrigin: "left bottom",
+      }}
+    />
+  )
+  
+  const LEFT_LEG = (
+    <div
+      style={{
         width: "100px",
         height: "10px",
         background: "black",
@@ -71,48 +80,48 @@ const LEFT_LEG = (
         top: "210px",
         right: 0,
         rotate: "-60deg",
-        transformOrigin: "right bottom"
-    }} />
-)
-
-const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
-
-type HangmanDrawingProps = {
+        transformOrigin: "right bottom",
+      }}
+    />
+  )
+  
+  const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+  
+  type HangmanDrawingProps = {
     numberOfGuesses: number
-}
-
-
-export const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
+  }
+  
+  export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     return (
-        <div style={{
-            position: "relative"
-        }}>
-            {BODY_PARTS.slice(0, numberOfGuesses)}
-            <div style={{
-                height: "50px",
-                width: "10px",
-                background: "black",
-                position: "absolute",
-                top: 0,
-                right: 0
-            }} />
-            <div style={{
-                height: "10px",
-                width: "200px",
-                background: "black",
-                marginLeft: "120px"
-            }} />
-            <div style={{
-                height: "400px",
-                width: "10px",
-                background: "black",
-                marginLeft: "120px"
-            }} />
-            <div style={{
-                height: "10px",
-                width: "250px",
-                background: "black"
-            }} />
-        </div>
+      <div style={{ position: "relative" }}>
+        {BODY_PARTS.slice(0, numberOfGuesses)}
+        <div
+          style={{
+            height: "50px",
+            width: "10px",
+            background: "black",
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        />
+        <div
+          style={{
+            height: "10px",
+            width: "200px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div
+          style={{
+            height: "400px",
+            width: "10px",
+            background: "black",
+            marginLeft: "120px",
+          }}
+        />
+        <div style={{ height: "10px", width: "250px", background: "black" }} />
+      </div>
     )
-}
+  }
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import styles from "../Keyboard.module.css"
-
 
 const KEYS = [
   "a",
@@ -31,22 +29,43 @@ const KEYS = [
   "z",
 ]
 
-const Keyboard = () => {
+type KeyboardProps = {
+  disabled?: boolean
+  activeLetters: string[]
+  inactiveLetters: string[]
+  addGuessedLetter: (letter: string) => void
+}
+
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
-      gap: ".5rem",
-    }}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
     >
       {KEYS.map(key => {
-        return <button className={`${styles.btn}`}
-          key={key}>
-          {key}
-        </button>
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        )
       })}
     </div>
   )
 }
-
-export default Keyboard
